31d7826bfba6df27cc2237fd84769d69cec4a5f5
Zanella-dev <krzanela@minha.fag.edu.br>
2026-05-27 19:44:02 -0300

    Organizado as lihas de código

--- src/script.js
+++ src/script.js
@@ -1,20 +1,17 @@
-// Captura o formulário quando o botão "Enviar Ticket" é clicado
 document.getElementById('contact-form').addEventListener('submit', async function(e) {
-  e.preventDefault(); // Evita que a página recarregue
+  e.preventDefault(); 
   
   const btn = document.getElementById('submit-btn');
   const responseDiv = document.getElementById('form-response');
   
-  // Muda o botão para mostrar que está carregando
   btn.innerText = 'Enviando...';
   btn.disabled = true;
 
-  // Pega os valores digitados
   const email = document.getElementById('email').value;
   const message = document.getElementById('message').value;
 
   try {
-    // Envia os dados para a nossa Netlify Function
+    
     const response = await fetch('/.netlify/functions/enviar-email', {
       method: 'POST',
       body: JSON.stringify({ email, message })
@@ -23,7 +20,7 @@ document.getElementById('contact-form').addEventListener('submit', async functio
     if (response.ok) {
       responseDiv.style.color = 'var(--neon-green)';
       responseDiv.innerText = 'Mensagem enviada com sucesso! GG WP.';
-      document.getElementById('contact-form').reset(); // Limpa os campos
+      document.getElementById('contact-form').reset(); 
     } else {
       throw new Error('Erro no servidor');
     }
@@ -31,7 +28,7 @@ document.getElementById('contact-form').addEventListener('submit', async functio
     responseDiv.style.color = '#ff3333';
     responseDiv.innerText = 'Ops! Algo deu errado ao enviar. Tente novamente.';
   } finally {
-    // Restaura o botão ao normal
+    
     responseDiv.style.display = 'block';
     btn.innerText = 'Enviar Ticket';
     btn.disabled = false;
